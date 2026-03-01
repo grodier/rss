b171d1a84acd377cedc9cf6bfe79f67dc7b6de4c
grodier <grodier3@gmail.com>
2026-02-28 20:09:41 -0500

    Parse command line args to config

--- cmd/api/application.go
+++ cmd/api/application.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"context"
+	"flag"
 	"log/slog"
 
 	"github.com/grodier/rss/internal/server"
 )
 
 type Application struct {
+	config config
 	logger *slog.Logger
 }
 
@@ -17,10 +19,30 @@ func NewApplication(logger *slog.Logger) *Application {
 	}
 }
 
+type config struct {
+	env    string
+	server serverConfig
+}
+
+type serverConfig struct {
+	port int
+}
+
+func defaultConfig() config {
+	return config{
+		env: "development",
+		server: serverConfig{
+			port: 8080,
+		},
+	}
+}
+
 func (app *Application) Run(ctx context.Context, args []string) error {
+	app.config = app.ParseConfigs(args)
+
 	srv := server.NewServer(app.logger)
-	srv.Port = 4000
-	srv.Env = "development"
+	srv.Port = app.config.server.port
+	srv.Env = app.config.env
 	srv.Version = version
 
 	if err := srv.Serve(); err != nil {
@@ -28,4 +50,22 @@ func (app *Application) Run(ctx context.Context, args []string) error {
 	}
 
 	return nil
+}
+
+func (app *Application) ParseConfigs(args []string) config {
+	config := defaultConfig()
+
+	fs := flag.NewFlagSet("rss-api", flag.ContinueOnError)
+
+	fs.StringVar(&config.env, "env", config.env, "Environment (development|production)")
+	fs.IntVar(&config.server.port, "port", config.server.port, "Server port")
+
+	fs.Parse(args)
+
+	if config.env != "development" && config.env != "production" {
+		app.logger.Warn("invalid environment value, falling back to default", "provided", config.env, "default", "development")
+		config.env = "development"
+	}
+
+	return config
 }

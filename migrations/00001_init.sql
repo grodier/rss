affa87df34faf0e94476b9747132328566ee1ed4
grodier <grodier3@gmail.com>
2026-09-04 20:07:19 -0400

    update user model

--- migrations/00001_init.sql
+++ migrations/00001_init.sql
@@ -1,9 +1,9 @@
 -- +goose Up
 CREATE TABLE users (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-
+    name TEXT NOT NULL,
     email TEXT NOT NULL UNIQUE,
-
+    hashed_password TEXT NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT now()
 );
 
